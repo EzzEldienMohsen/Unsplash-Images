5aa973fc9aeaa6f1ff9cf4b5d4134d35fd40dacb
Ezz dghedy <ezzmohsend@gmail.com>
2024-01-30 17:22:51 +0200

    applying infinite query

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { AppProvider } from '../components/Context'
-import 'react-toastify/dist/ReactToastify.css'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-var queryClient = new QueryClient()
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { AppProvider } from '../components/Context';
+import 'react-toastify/dist/ReactToastify.css';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+var queryClient = new QueryClient();
+// queryClient.setQueryData(['images'], (data) => ({
+//   pages: data.pages.slice(1),
+//   pageParams: data.pageParams.slice(1),
+// }));
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AppProvider>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </AppProvider>
-)
+);
